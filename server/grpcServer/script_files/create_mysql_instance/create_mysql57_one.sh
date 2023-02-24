4efc2f393a53aea9ac743ab13f1e20c1ecfeb5ee
wqmysqldba@126.com <wqmysqldba@126.com>
2023-02-24 21:04:32 +0800

    #    Functions: 1.用于实现5.7版本,MySQL数据库,单个实例的创建 #               2.数据库初始用户创建 项目dba: proj_dba,复制用户:repl_user,备份用户:backup_user #               3.开启cron任务，mysql数据库日志的按天分割 # 4、node_exporter安装

--- server/grpcServer/script_files/create_mysql_instance/create_mysql57_one.sh
+++ server/grpcServer/script_files/create_mysql_instance/create_mysql57_one.sh
@@ -8,9 +8,11 @@
 #    Functions: 1.用于实现5.7版本,MySQL数据库,单个实例的创建
 #               2.数据库初始用户创建 项目dba: proj_dba,复制用户:repl_user,备份用户:backup_user
 #               3.开启cron任务，mysql数据库日志的按天分割
+#               4.开始MySQL数据库监控
 #    exit code: 0 正常退出脚本
 #               100 数据库端口已占用
 #               101 数据库启动失败
+#               102 目标端口被占用且无可用端口
 #               200 未知错误
 #===========================================================================================
 
@@ -19,19 +21,21 @@ function log() {
 }
 
 function parseopt() {
-
-	TEMP=$(getopt -o v:p:r:R:b:B:m:M:i:P:d:s:S:H: \
-		--long version:,passwd:,repl_user:,repl_user_passwd:,backup_user:,backup_user_passwd:,max_memory:,max_connections:, hub_ip:,hub_port:,hub_dir:,hub_ssh_user:,hub_ssh_user_passwd:,host_id: \
-		-n 'create_mysql57_one.sh' -- "$@")
+	if [ $# -lt 10 ]; then
+		echo "wrong options"
+		print_usage
+		exit 1
+	fi
+	TEMP=$(getopt -o v:p:m:M:i:P:d:s:S:H: --long version:,port:,max_memory:,max_connections:,hub_ip:,hub_port:,hub_dir:,hub_ssh_user:,hub_ssh_user_passwd:,host_id: -- "$@")
 	if [ $? -ne 0 ]; then
 		echo "Terminating..." >&2
+		print_usage
 		on_failure 1
 	fi
 	eval set -- "$TEMP"
 	while true; do
 		case "$1" in
 		-v | --version)
-			# shellcheck disable=SC2034
 			version="$2"
 			shift 2
 			;;
@@ -39,41 +43,37 @@ function parseopt() {
 			port="$2"
 			shift 2
 			;;
-		-r | --repl_user)
-			repl_user="$2"
-			;;
-		-R | --repl_user_passwd)
-			repl_user_passwd="$2"
-			;;
-		-b | --backup_user)
-			backup_user="$2"
-			;;
-		-B | --backup_user_passwd)
-			backup_user_passwd="$2"
-			;;
 		-m | --max_memory)
 			max_memory="$2"
+			shift 2
 			;;
 		-M | --max_connections)
 			max_connections="$2"
+			shift 2
 			;;
 		-i | --hub_ip)
 			hub_ip="$2"
+			shift 2
 			;;
 		-P | --hub_port)
 			hub_port="$2"
+			shift 2
 			;;
 		-d | --hub_dir)
 			hub_dir="$2"
+			shift 2
 			;;
 		-s | --hub_ssh_user)
 			hub_ssh_user="$2"
+			shift 2
 			;;
 		-S | --hub_ssh_user_passwd)
 			hub_ssh_user_passwd="$2"
+			shift 2
 			;;
 		-H | --host_id)
 			host_id="$2"
+			shift 2
 			;;
 		--)
 			shift
@@ -90,56 +90,57 @@ function parseopt() {
 
 # shellcheck disable=SC2120
 function print_usage() {
+	on_debug
 	echo "USAGE:"
 	echo "
-    -v | --version)
-		-p | --port)
-		-r | --repl_user)
-	  -R | --repl_user_passwd)
-	  -b | --backup_user)
-	  -B | --backup_user_passwd)
-	  -m | --max_memory)
-	  -M | --max_connections)
-	  -i | --hub_ip)
-	  -P | --hub_port)
-	  -d | --hub_dir)
-	  -s | --hub_ssh_user)
-	  -S | --hub_ssh_user_passwd)
-	  -H | --host_id)
+-v | --version)
+-p | --port)
+-m | --max_memory)
+-M | --max_connections)
+-i | --hub_ip)
+-P | --hub_port)
+-d | --hub_dir)
+-s | --hub_ssh_user)
+-S | --hub_ssh_user_passwd)
+-H | --host_id)
     "
 }
 # shellcheck disable=SC2120
 function download_mysql_package() {
-	remote_basedir_File=/home/saasdb_db_package/mysql/57/${version}/basedir
+	remote_basedir_File=${hub_dir}/mysql/57/${version}/basedir
 	# 包含datadir和特定的用户授权信息
-	remote_datadir_File=/home/saasdb_db_package/mysql/57/${version}/datadir
+	remote_datadir_File=${hub_dir}/mysql/57/${version}/datadir
 
 	#修改配置文件，如果是需要binlog，serverid取saasdb数据库中saas_instance表的primary key
-	local_base_dir=/root/mysql_base_dir/${version}
+	local_base_dir=/usr/local/mysql_base_dir
 	local_data_dir=/data/mysql/${port}
+	mkdir -p ${local_base_dir}
+	mkdir -p ${local_data_dir}
+
 	# 配置文件
 	local_mysqld_conf=${local_data_dir}/conf/my.cnf
 	#判断本地是否含有basedir
 	# 不存在basedir就
-	if [ ! -d "$remote_basedir_File" ]; then
+	yum install -y expect
+	if [ -d "${local_base_dir}/${version}" ]; then
+		echo "目录存在"
+	else
 		echo "目录不存在"
 		expect <<EOF
        set timeout 1200;
        spawn scp -P${hub_port} -r -p ${hub_ssh_user}@${hub_ip}:"${remote_basedir_File}" "${local_base_dir}"
-       expect{
+       expect {
          "*yes/no*" {send "yes\n";exp_continue}
          "*Permission denied*" {exit 1}
          "*password*" {send "${hub_ssh_user_passwd}\n";exp_continue}
          "*Killed by signal 1" {exit 1}
        }
 EOF
-	else
-		echo "未知错误"
-		on_failure 200
+		cd ${local_base_dir} && mv basedir $version
 	fi
 
 	#判断本地是否含有datadir
-	if [ -d "$remote_basedir_File" ]; then
+	if [ -d "${local_data_dir}" ]; then
 		log "${local_data_dir} 存在，备份该目录"
 		day=$(date +"%Y-%m-%d")
 		mv "${local_data_dir}" "${local_data_dir}"_"${day}"
@@ -149,7 +150,7 @@ EOF
 	expect <<EOF
        set timeout 1200;
        spawn scp -P${hub_port} -r -p ${hub_ssh_user}@${hub_ip}:"${remote_datadir_File}" "${local_data_dir}"
-       expect{
+       expect {
          "*yes/no*" {send "yes\n";exp_continue}
          "*Permission denied*" {exit 1}
          "*password*" {send "${hub_ssh_user_passwd}\n";exp_continue}
@@ -157,25 +158,39 @@ EOF
          }
 EOF
 
+	# 授权
+	id mysql
+	if [ $? -ne 0 ]; then
+		useradd mysql
+	fi
+	chown -R mysql:mysql ${local_base_dir}
+	chown -R mysql:mysql ${local_data_dir}
+
 }
 
 function start_mysql() {
 	# 不需要确认文件属主
 	# TODO 调整为使用 tcmalloc
-
-	${local_base_dir}/bin/mysqld_safe --defaults-file=${local_mysqld_conf} --user=mysql &
+	${local_base_dir}/${version}/bin/mysqld_safe --defaults-file=${local_mysqld_conf} --user=mysql &
 	sleep 10
 	ps -ef | grep -w mysqld_safe | grep ${port} | grep -v grep
-	if [ $? -eq 1]; then
+	if [ $? -eq 1 ]; then
 		log "${port} 数据库启动失败"
 		on_failure 101
 	fi
 }
 
-function stop_mysql() {
-	mysqld_safe_pid=$(ps -ef | grep -w mysqld_safe | grep ${port} | grep -v grep | awk -F '{print $2}')
-	mysqld_pid=$(ps -ef | grep -w mysqld | grep ${port} | grep -v grep | awk -F '{print $2}')
+### kill mysqld
+function kill_mysql() {
+	mysqld_safe_pid=$(ps -ef | grep -w mysqld_safe | grep ${port} | grep -v grep | awk '{print $2}')
+	mysqld_pid=$(ps -ef | grep -w mysqld | grep ${port} | grep -v grep | awk '{print $2}')
 	kill -9 ${mysqld_safe_pid} ${mysqld_pid}
+}
+
+### stop mysqld
+
+function stop_mysql() {
+	${local_base_dir}/${version}/bin/mysqladmin --user="root" -p${default_root_passwd} -S ${local_data_dir}/data/sock/mysql.sock shutdown
 }
 
 # func init_conf
@@ -184,7 +199,7 @@ function stop_mysql() {
 # server_id
 # auto.cnf
 # 会话级别的参数
-# max_connection 参数
+# max_connections 参数
 # buffer pool 参数
 # 和涉及到datadir前缀相关的
 # 的替换
@@ -192,27 +207,27 @@ function stop_mysql() {
 function init_conf() {
 
 	port_placeholder="PORT_PLACEHOLDER"
-	sed -i "s/${port}/${port_placeholder}/g" ${local_mysqld_conf}
+	sed -i "s/${port_placeholder}/${port}/g" ${local_mysqld_conf}
 
 	# server_id
 	server_id_placeholder="SERVER_ID_PLACEHOLDER"
-	sed -i "s/${host_id}/${server_id_placeholder}/g" ${local_mysqld_conf}
+	sed -i "s/${server_id_placeholder}/${host_id}/g" ${local_mysqld_conf}
 
 	# max_connection
-	max_connection_placeholder="MAX_CONN_PLACEHOLDER"
-	sed -i "s/${max_connection}/${max_connection_placeholder}/g" ${local_mysqld_conf}
+	max_connections_placeholder="MAX_CONN_PLACEHOLDER"
+	sed -i "s/${max_connections_placeholder}/${max_connections}/g" ${local_mysqld_conf}
 
 	# innodb_buffer_pool
 	innodb_buffer_pool_size=$((${max_memory} / 8 * 10))
 	innodb_buffer_pool_placeholder="INNODB_BUFFER_POOL_SIEZ_PLACEHOLDER"
-	sed -i "s/${innodb_buffer_pool_size}/${innodb_buffer_pool_placeholder}/g" ${local_mysqld_conf}
+	sed -i "s/${innodb_buffer_pool_placeholder}/${innodb_buffer_pool_size}G/g" ${local_mysqld_conf}
 
 	#prefix替换
 	config_file_prefix="PREFIX"
-	sed -i "s/${local_data_dir}/${config_file_prefix}/g" ${local_mysqld_conf}
+	sed -i "s#${config_file_prefix}#${local_data_dir}#g" ${local_mysqld_conf}
 
 	if [ ${max_memory} -lt 16 ]; then
-		sort_buffer_size=256K
+		sort_buffer_size=128K
 		join_buffer_size=128K
 		read_buffer_size=128K
 		read_rnd_buffer_size=256K
@@ -224,37 +239,56 @@ function init_conf() {
 		join_buffer_size=1M
 		read_buffer_size=1M
 		read_rnd_buffer_size=1M
-		tmp_table_size=32M
+		tmp_table_size=4M
 		bulk_insert_buffer_size=2M
 		thread_stack=128K
 	fi
 	sort_buffer_size_placeholder="SORT_BUFFER_SIZE_PLACEHOLDER"
 	join_buffer_size_placeholder="JOIN_BUFFER_SIZE_PLACEHOLDER"
 	read_buffer_size_placeholder="READ_BUFFER_SIZE_PLACEHOLDER"
+	read_rnd_buffer_size_placeholder="READ_RND_BUFFER_SIZE_PLACEHOLDER"
 	tmp_table_size_placeholder="TMP_TABLE_SIZE_PLACEHOLDER"
 	bulk_insert_buffer_size_placeholder="BULK_INSERT_BUFFER_SIZE_PLACEHOLDER"
-	thread_stack_placeholder="THREAD_STACK"
+	thread_stack_placeholder="THREAD_STACK_PLACEHOLDER"
 
-	sed -i "s/${sort_buffer_size}/${sort_buffer_size_placeholder} g" ${local_mysqld_conf}
-	sed -i "s/${join_buffer_size}/${join_buffer_size_placeholder} g" ${local_mysqld_conf}
-	sed -i "s/${read_buffer_size}/${read_buffer_size_placeholder} g" ${local_mysqld_conf}
-	sed -i "s/${tmp_table_size}/${tmp_table_size_placeholder} g" ${local_mysqld_conf}
-	sed -i "s/${bulk_insert_buffer_size}/${bulk_insert_buffer_size_placeholder} g" ${local_mysqld_conf}
-	sed -i "s/${thread_stack}/${thread_stack_placeholder} g" ${local_mysqld_conf}
+	sed -i "s/${sort_buffer_size_placeholder}/${sort_buffer_size}/g" ${local_mysqld_conf}
+	sed -i "s/${join_buffer_size_placeholder}/${join_buffer_size}/g" ${local_mysqld_conf}
+	sed -i "s/${read_buffer_size_placeholder}/${read_buffer_size}/g" ${local_mysqld_conf}
+	sed -i "s/${read_rnd_buffer_size_placeholder}/${read_rnd_buffer_size}/g" ${local_mysqld_conf}
+	sed -i "s/${tmp_table_size_placeholder}/${tmp_table_size}/g" ${local_mysqld_conf}
+	sed -i "s/${bulk_insert_buffer_size_placeholder}/${bulk_insert_buffer_size}/g" ${local_mysqld_conf}
+	sed -i "s/${thread_stack_placeholder}/${thread_stack}/g" ${local_mysqld_conf}
 
 }
 function init_mysql() {
-  scripts_dir=${local_data_dir}/scripts
-  default_root_passwd=$(cat ${scripts_dir}/root.cnf)
-	conn_local_mysql="${local_base_dir}/bin/mysql -uroot -p${default_root_passwd} "
+	scripts_dir=${local_data_dir}/script_files
+	default_root_passwd=$(cat ${scripts_dir}/root.cnf)
+	create_default_user_sql=${scripts_dir}/sql/default_users.sql
 
-	${conn_local_mysql} < default_users.sql
+	conn_local_mysql="${local_base_dir}/$version/bin/mysql -uroot -p${default_root_passwd} -S ${local_data_dir}/data/sock/mysql.sock  "
+
+	${conn_local_mysql} <${create_default_user_sql}
 	# 清理binlog保证数据库无事务信息
 	${conn_local_mysql} -e "RESET MASTER"
 }
+
+function add_comment_to_semi() {
+	semi_flag="loose-rpl_semi_sync_"
+	sed -i "/${semi_flag}/ s/^/#/" ${local_mysqld_conf}
+
+}
+
+function clear_comment_to_semi() {
+	semi_flag="loose-rpl_semi_sync_"
+	sed -i "/${semi_flag}/ s/^#//" ${local_mysqld_conf}
+}
+
 # shellcheck disable=SC2120
 function main() {
+	set -e
 	parseopt "$@"
+	on_debug
+
 	#检查是否存在端口占用
 	if lsof -Pi :"${port}" -sTCP:LISTEN -t >/dev/null; then
 		log "Port is in use"
@@ -266,11 +300,106 @@ function main() {
 	download_mysql_package
 	# 初始化配置文件
 	init_conf
+	add_comment_to_semi
 	# 启动数据库资源
 	start_mysql
 	# 完成init
 	init_mysql
+	kill_mysql
+	clear_comment_to_semi
+	start_mysql
+	add_monitor
 	# 完成
+	on_success
+
+}
+
+function on_debug() {
+	log "/bin/bash $0
+--port ${port}
+--max_memory ${max_memory}
+--max_connections ${max_connections}
+--hub_ip ${hub_ip}
+--hub_port ${hub_port}
+--hub_dir ${hub_dir}
+--hub_ssh_user ${hub_ssh_user}
+--hub_ssh_user_passwd ${hub_ssh_user_passwd}
+--host_id ${host_id}"
+}
+
+# 下载exporter
+function download_exporter_package() {
+
+	#判断本地是否含有monitor文件
+	yum install -y expect
+	if [ -d "${local_monitor_dir}/node_exporter" ]; then
+		echo "目录存在"
+	else
+		echo "目录不存在"
+		expect <<EOF
+       set timeout 1200;
+       spawn scp -P${hub_port} -r -p ${hub_ssh_user}@${hub_ip}:"${remote_monitor_File}" "${local_monitor_base}"
+       expect {
+         "*yes/no*" {send "yes\n";exp_continue}
+         "*Permission denied*" {exit 1}
+         "*password*" {send "${hub_ssh_user_passwd}\n";exp_continue}
+         "*Killed by signal 1" {exit 1}
+       }
+EOF
+	fi
+}
+
+function run_exporter() {
+
+	if [ $(check_process_exists "node_exporter") -eq 1 ]; then
+		# 判断9100端口是否被占用
+		local port=9100
+		if lsof -Pi :$port -sTCP:LISTEN -t >/dev/null; then
+			# 端口被占用
+			pid=$(netstat -tlnp | grep ":$port" | awk '{print $NF}' | awk -F'/' '{print $1}')
+			log "Port $port is occupied by PID $pid."
+			# 获取比目标端口大但最接近目标端口的空闲端口
+			available_port=$(ss -tnl | awk '{print $4}' | awk -F':' '{print $NF}' | sort -rn | awk -v port=$port '$1 > port {print $1; exit}' | awk 'END{print ($0 == "") ? port + 1 : $0 + 1}')
+			echo "Try to start service on port $new_port instead."
+		else
+			# 9100端口未被占用
+			available_port=$port
+		fi
+		echo "Starting node_exporter on port $available_port"
+		NODE_EXPORT_BIN="${local_monitor_dir}/node_exporter/node_exporter"
+		exec ${NODE_EXPORT_BIN} \
+			--web.listen-address=:$available_port \
+			--collector.tcpstat \
+			--collector.systemd \
+			--collector.mountstats \
+			--collector.meminfo_numa \
+			--collector.interrupts \
+			--collector.buddyinfo \
+			--collector.vmstat.fields="^.*" \
+			--log.level="info" &
+	fi
+
+	# TODO 如何持久化这部分内容
+	# TODO 如何保证如果有节点故障，新来的exporter不会占用这个断端口
+}
+
+function check_process_exists() {
+	if pidof -x "进程名" >/dev/null; then
+		echo 0
+	else
+		echo 1
+	fi
+}
+
+#Add database monitoring
+function add_monitor() {
+	remote_monitor_File=${hub_dir}/monitor/mysql_monitor
+	local_monitor_base=/root
+	local_monitor_dir=/root/mysql_monitor
+	mkdir -p ${local_monitor_dir}
+	download_exporter_package
+	run_exporter
+
 }
 
 function on_success() {
